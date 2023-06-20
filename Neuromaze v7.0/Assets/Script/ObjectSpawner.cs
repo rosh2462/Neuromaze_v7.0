67be03bb4d2426b911ea070a6860af56590b9166
IbrvCvrtxxn <iibrahim.ismayilovv@gmail.com>
2023-06-20 02:33:43 +0200

    small_fix

--- Neuromaze v7.0/Assets/Script/ObjectSpawner.cs
+++ Neuromaze v7.0/Assets/Script/ObjectSpawner.cs
@@ -1,4 +1,56 @@
 
+// using UnityEngine;
+
+// public class ObjectSpawner : MonoBehaviour
+// {
+//     public GameObject objectPrefab;
+//     public float spawnInterval = 10f;
+//     public int spawnNumber = 5; 
+//     public Vector3 spawnRotation = Vector3.zero; 
+//     public GameObject particleSystemPrefab;
+//     public float delayAfterParticleSystem = 2.1f; // Delay in seconds after the particle system starts before the object appears
+
+//     private void Start()
+//     {
+//         if (spawnNumber > 0)
+//         {
+//             // Spawn the first object immediately
+//             SpawnObject();
+            
+//             // Start spawning objects with the given interval, but only if spawnNumber is greater than 1
+//             if (spawnNumber > 1)
+//             {
+//                 InvokeRepeating("SpawnObject", spawnInterval, spawnInterval);
+//             }
+//         }
+//     }
+
+//     private void SpawnObject()
+//     {
+//         // Instantiate the particle system prefab
+//         GameObject particleSystemObject = Instantiate(particleSystemPrefab, transform.position, Quaternion.identity);
+
+//         // Invoke the method to spawn the object with the specified delay
+//         Invoke("InstantiateObject", delayAfterParticleSystem);
+//     }
+
+//     private void InstantiateObject()
+//     {
+//         // Instantiate the object prefab
+//         GameObject spawnedObject = Instantiate(objectPrefab, transform.position, Quaternion.Euler(spawnRotation));
+
+//         // Decrease the count of objects to spawn
+//         spawnNumber--;
+
+//         // If all objects have been spawned, cancel the spawning
+//         if (spawnNumber <= 0)
+//         {
+//             CancelInvoke("SpawnObject");
+//         }
+//     }
+// }
+
+
 using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
@@ -12,17 +64,8 @@ public class ObjectSpawner : MonoBehaviour
 
     private void Start()
     {
-        if (spawnNumber > 0)
-        {
-            // Spawn the first object immediately
-            SpawnObject();
-            
-            // Start spawning objects with the given interval, but only if spawnNumber is greater than 1
-            if (spawnNumber > 1)
-            {
-                InvokeRepeating("SpawnObject", spawnInterval, spawnInterval);
-            }
-        }
+        // Start spawning objects with the given interval
+        InvokeRepeating("SpawnObject", spawnInterval, spawnInterval);
     }
 
     private void SpawnObject()
