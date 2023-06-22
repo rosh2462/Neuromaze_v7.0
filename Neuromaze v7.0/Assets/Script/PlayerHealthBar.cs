54f833627fc21195c9289770d9991c1c401fdc65
IbrvCvrtxxn <iibrahim.ismayilovv@gmail.com>
2023-06-22 23:04:04 +0200

    bloodyScreen_portalColor

--- Neuromaze v7.0/Assets/Script/PlayerHealthBar.cs
+++ Neuromaze v7.0/Assets/Script/PlayerHealthBar.cs
@@ -5,64 +5,65 @@
 
 // public class PlayerHealthBar : MonoBehaviour
 // {
+//     public int maxHealth = 100;
+//     public int currentHealth;
 
-// 	public int maxHealth = 100;
-// 	public int currentHealth;
+//     public HealthBar healthBar;
 
-// 	public HealthBar healthBar;
+//     public GameObject gameOverWindow;
+//     bool gameIsOver = false;
 
-// 	public GameObject gameOverWindow;
-// 	bool gameIsOver = false;
-
-//     // Start is called before the first frame update
-//     void Start()
+//     private void Start()
 //     {
-// 		gameOverWindow.SetActive(false);
-
-// 		currentHealth = maxHealth;
-// 		healthBar.SetMaxHealth(maxHealth);
+//         gameOverWindow.SetActive(false);
+//         currentHealth = maxHealth;
+//         healthBar.SetMaxHealth(maxHealth);
 //     }
 
-//     // Update is called once per frame
-//     void Update()
+//     private void Update()
 //     {
-// 		if (Input.GetKeyDown(KeyCode.F2))
-// 		{
-// 			TakeDamage(5);
-// 		}
+//         if (Input.GetKeyDown(KeyCode.F2))
+//         {
+//             TakeDamage(5);
+//         }
 
-// 		if (currentHealth <= 0 && !gameIsOver)
+//         if (currentHealth <= 0 && !gameIsOver)
 //         {
 //             GameOver();
 //         }
 //     }
 
-// 	void TakeDamage(int damage)
-// 	{
-// 		currentHealth -= damage;
+//     public void TakeDamage(int damage)
+//     {
+//         currentHealth -= damage;
+//         healthBar.SetHealth(currentHealth);
+//     }
 
-// 		healthBar.SetHealth(currentHealth);
-// 	}
+//     public void IncreaseHealth(int amount)
+//     {
+//         currentHealth += amount;
+//         if (currentHealth > maxHealth)
+//         {
+//             currentHealth = maxHealth;
+//         }
+//         healthBar.SetHealth(currentHealth);
+//     }
 
-
-// 	void GameOver()
+//     private void GameOver()
 //     {
 //         gameIsOver = true;
 //         gameOverWindow.SetActive(true);
 //         Time.timeScale = 0;
-
 //         Cursor.visible = true;
 //     }
 
-// 	public void RestartScene()
+//     public void RestartScene()
 //     {
 //         Time.timeScale = 1;
-        
 //         gameIsOver = false;
 //         gameOverWindow.SetActive(false);
 //         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 //     }
-	
 // }
 
 
@@ -70,6 +71,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealthBar : MonoBehaviour
 {
@@ -77,15 +79,22 @@ public class PlayerHealthBar : MonoBehaviour
     public int currentHealth;
 
     public HealthBar healthBar;
+    public GameObject damageEffectPrefab; // Prefab of the game object with sound effect
 
     public GameObject gameOverWindow;
+    public AudioSource gameOverSound;
+    public CanvasGroup canvasGroup; // Reference to the CanvasGroup component
     bool gameIsOver = false;
+    bool tookDamage = false; // Variable to track if damage was taken in the current frame
+    float fadeSpeed = 1.5f; // Speed at which the canvas fades
 
     private void Start()
     {
         gameOverWindow.SetActive(false);
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+
+        canvasGroup.alpha = 0f;        
     }
 
     private void Update()
@@ -99,12 +108,50 @@ public class PlayerHealthBar : MonoBehaviour
         {
             GameOver();
         }
+
+        if (tookDamage)
+        {
+            tookDamage = false;
+            StartCoroutine(FadeCanvasInAndOut());
+
+            canvasGroup.alpha = 0.5f;
+        }
+
+        if(currentHealth >= 30 && !gameIsOver)
+        {
+            canvasGroup.alpha = 0f;
+        }
+        else
+        {
+            StartCoroutine(FadeCanvasInAndOut());
+            canvasGroup.alpha = 1f;
+        }
+
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+
+        if (damageEffectPrefab != null)
+        {
+            GameObject damageEffect = Instantiate(damageEffectPrefab, transform.position, Quaternion.identity);
+            AudioSource audioSource = damageEffect.GetComponent<AudioSource>();
+            if (audioSource != null)
+            {
+                StartCoroutine(PlayDelayedSoundEffect(audioSource));
+            }
+        }
+
+        tookDamage = true;
+    }
+
+    private IEnumerator PlayDelayedSoundEffect(AudioSource audioSource)
+    {
+        yield return new WaitForSeconds(0.2f); // Delay before playing the sound effect
+
+        audioSource.Play();
     }
 
     public void IncreaseHealth(int amount)
@@ -123,6 +170,11 @@ public class PlayerHealthBar : MonoBehaviour
         gameOverWindow.SetActive(true);
         Time.timeScale = 0;
         Cursor.visible = true;
+
+        if (gameOverSound != null)
+        {
+            gameOverSound.Play(); // Play the game over sound effect
+        }
     }
 
     public void RestartScene()
@@ -131,5 +183,24 @@ public class PlayerHealthBar : MonoBehaviour
         gameIsOver = false;
         gameOverWindow.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator FadeCanvasInAndOut()
+    {
+        canvasGroup.alpha = 1f; // Set initial alpha to fully opaque
+
+        yield return new WaitForSeconds(0.3f);
+
+        float currentTime = 0f;
+
+        while (currentTime < fadeSpeed)
+        {
+            currentTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / fadeSpeed); // Calculate interpolated alpha value
+            canvasGroup.alpha = alpha;
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f; // Set final alpha to fully transparent
     }
 }
